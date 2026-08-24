cde2a7b55d301bf0955edd50d0708784744f6520
Princy <ramarokotoprincy@gmail.com>
2026-08-24 12:50:32 +0300

    migration to vite

--- public/operators.d.ts
+++
@@ -1,4 +0,0 @@
-export interface State {
-    input: string[];
-}
-//# sourceMappingURL=operators.d.ts.map
--- src/calculator.ts
+++ src/calculator.ts
@@ -1,20 +1,66 @@
-
-interface State {
-    input : string[],
-}
+import "./style.css";
 
 
-let calculatorState: State = {
-  input: [],
-};
 
 const buttons = document.querySelectorAll("button");
 const result = document.querySelector(".result") as HTMLTextAreaElement;
 
+function calc(): string[] {
+
+  const regex = /[+*%/\-]/g;
+
+  let numberValues = result.value.split(regex);
+  let operators = result.value.match(regex)!;
+  let tempResult = 0;
+
+  while (numberValues.length !== 1) {
+    for (let i = 0; i < operators.length; i++) {
+      if (operators[i] === "*") {
+        tempResult =
+          parseFloat(numberValues[i]!) * parseFloat(numberValues[i + 1]!);
+        numberValues.splice(i, 2, `${tempResult}`);
+        operators.splice(i, 1);
+      }
+      if (operators[i] === "/") {
+        tempResult =
+          parseFloat(numberValues[i]!) / parseFloat(numberValues[i + 1]!);
+        numberValues.splice(i, 2, `${tempResult}`);
+        operators.splice(i, 1);
+      }
+      if (operators[i] === "%") {
+        tempResult =
+          (parseFloat(numberValues[i]!) / 100) *
+          parseFloat(numberValues[i + 1]!);
+
+        numberValues.splice(i, 2, `${tempResult}`);
+        operators.splice(i, 1);
+        i--;
+      }
+    }
+
+    for (let i = 0; i < operators.length; i++) {
+      if (operators[i] === "+") {
+        tempResult =
+          parseFloat(numberValues[i]!) + parseFloat(numberValues[i + 1]!);
+        numberValues.splice(i, 2, `${tempResult}`);
+        operators.splice(i, 1);
+      }
+      if (operators[i] === "-") {
+        tempResult =
+          parseFloat(numberValues[i]!) - parseFloat(numberValues[i + 1]!);
+        numberValues.splice(i, 2, `${tempResult}`);
+        operators.splice(i, 1);
+      }
+    }
+  }
+
+  return numberValues;
+}
+
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
     if (button.value === "=") {
-      result.value = String(calc(calculatorState));
+      result.value = String(calc());
     } else if (button.value === "reset") {
       reset();
     } else if (button.value === "del") {
@@ -26,48 +72,6 @@ buttons.forEach((button) => {
 });
 
 function reset(): void {
-  calculatorState = {
-    input: [],
-  };
+
   result.value = "";
-}
-
-function calc(calculatorState: State) :string[]{
-  const regex = /[+*/\-]/g;
-
-  let numberValues  = result.value.split(regex);
-  let operators = result.value.match(regex)!;
-  let tempResult = 0;
-
-  while (numberValues.length !== 1){
-    for(let i = 0; i < operators.length ; i++){
-      if(operators[i] === "*"){     
-      tempResult =  parseFloat(numberValues[i]!)*parseFloat(numberValues[i+1]!);
-      numberValues.splice(i,2,`${tempResult}`)
-      operators.splice(i,1);
-      }
-      if(operators[i] === "/"){     
-      tempResult =  parseFloat(numberValues[i]!)/parseFloat(numberValues[i+1]!);
-      numberValues.splice(i,2,`${tempResult}`)
-      operators.splice(i,1);
-      }
-    }
-
-     for(let i =0;i<operators.length;i++){ 
-      if(operators[i] === "+"){     
-      tempResult =  parseFloat(numberValues[i]!)+parseFloat(numberValues[i+1]!);
-      numberValues.splice(i,2,`${tempResult}`)
-      operators.splice(i,1);
-      }
-      if(operators[i] === "-"){     
-      tempResult =  parseFloat(numberValues[i]!)-parseFloat(numberValues[i+1]!);
-      numberValues.splice(i,2,`${tempResult}`)
-      operators.splice(i,1);
-      }
-    }
-  } 
- 
-
-
-  return numberValues;
 }
